357c33f78a387f56a7afc7a695ef7ee330546fa9
Johan Gabriel <johan.universidade@gmail.com>
2026-03-26 20:03:03 -0300

    Rest realizado

--- clientes/src/main/java/com/johan/clientes/controller/ClienteController.java
+++ clientes/src/main/java/com/johan/clientes/controller/ClienteController.java
@@ -3,35 +3,50 @@ package com.johan.clientes.controller;
 import com.johan.clientes.models.ClienteModel;
 import com.johan.clientes.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/clientes")
+@RequestMapping(path = "/clientes")
 public class ClienteController {
 
     @Autowired
     private ClienteService clienteService;
 
+
+
     @PostMapping
-    public ClienteModel criarCliente(@RequestBody ClienteModel clienteModel){
-        return clienteService.criarCliente(clienteModel);
+    public ResponseEntity<ClienteModel> criarCliente(@RequestBody ClienteModel clienteModel){
+
+        ClienteModel request = clienteService.criarCliente(clienteModel);
+
+        URI uri = URI.create("/clientes/" + request.getId());
+        return ResponseEntity.created(uri).body(request);
     }
 
     @GetMapping
-    public List<ClienteModel> listarClientes(){
-        return clienteService.findAll();
+    public ResponseEntity<List<ClienteModel>> findAll(){
+        List<ClienteModel> request = clienteService.findAll();
+        return ResponseEntity.ok().body(request);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarCliente(@PathVariable Long id){
+        clienteService.deletarCliente(id);
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/{id}")
-    public Optional<ClienteModel> buscarClientePorId(@PathVariable Long id){
-        return clienteService.buscarClientePorId(id);
+    public void buscarClientePorId(@PathVariable Long id){
+        clienteService.buscarClientePorId(id);
     }
 
-    @DeleteMapping("/{id}")
-    public void deletarCliente(@PathVariable Long id){
-        clienteService.deletarCliente(id);
-    }
+
+
+
 }
